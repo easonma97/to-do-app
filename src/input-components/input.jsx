1ea66c51314b96ddcb6619f7313d488b54c3b860
easonma97 <a705193483@gmail.com>
2023-03-07 11:07:21 +1030

    Adjust the structure

--- src/input-components/input.jsx
+++ src/input-components/input.jsx
@@ -1,9 +1,24 @@
-function input({setToDos, handleSubmit}) {
-    return <form>
+import React from "react";
+import { useState } from "react";
+
+function Input({setToDos}) {
+    const [inputText, setInputText] = useState('');
+
+    function handeInputText(event) {
+        setInputText(event.target.value);
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        const Post = inputText;
+        setToDos((existingPosts) => [Post, ...existingPosts]);
+      }
+
+    return <form onSubmit={handleSubmit}>
         <label>Please input things to do: </label>
-        <input type="text" onChange={setToDos}/>
-        <button onClick={handleSubmit}>Add Task</button>
-    </form>
+        <input type="text" onChange={handeInputText}/>
+        <button>Add Task</button>
+    </form> 
 }
 
-export default input;
+export default Input;
