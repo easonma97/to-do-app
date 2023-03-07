f18bb19cab443c79d4936df6e3bd09ce33501ef0
easonma97 <a705193483@gmail.com>
2023-03-07 17:42:27 +1030

    add some features[checkbox, delete button]

--- src/input-components/input.jsx
+++ src/input-components/input.jsx
@@ -1,23 +1,30 @@
 import React from "react";
-import { useState } from "react";
 
-function Input({setToDos}) {
-    const [inputText, setInputText] = useState('');
-
+function Input({setToDos, inputText, setInputText}) {
     function handeInputText(event) {
-        setInputText(event.target.value);
+        setInputText({content: event.target.value});
+    }
+
+    function handleInputPriority(event) {
+        setInputText({priority: event.target.value});
     }
 
     function handleSubmit(event){
         event.preventDefault();
         const Post = inputText;
         setToDos((existingPosts) => [Post, ...existingPosts]);
-        setInputText('');
+        setInputText({content: ''});
       }
 
     return <form onSubmit={handleSubmit}>
         <label>Please input things to do: </label>
-        <input type="text" value={inputText} onChange={handeInputText}/>
+        <input type="text" value={inputText.content} onChange={handeInputText}/><br />
+        <label>How important the thing is?</label>
+        <select onChange={ }>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+        </select>
         <button>Add Task</button>
     </form> 
 }
